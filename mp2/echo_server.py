b2675d29463c92496f60b1644924d89c955f97d5
anastasia578 <nastia@home>
2021-11-05 14:39:43 -0400

    adding

---
+++ mp2/echo_server.py
@@ -0,0 +1,50 @@
+import socket
+import argparse
+
+# Building an echo server.
+# Echo service will listen on a user-specified port.
+# When a client connects to your echo service on this port, your service will send back to the client
+# any data it receives.
+# Your service should continue to echo back until the client closes the connection.
+# Your code should support "reverse" mode - the reverse of the received data is returned to the client.
+# Your recv buffer should be at least 1024 bytes.
+# After the client closes the connection, your echo service should go back to its wait state, where it awaits a
+# new connection for a client.
+
+def echo_service(port, reverse):
+    print("Port: %d" % port)
+    if(reverse):
+        print("reverse")
+    else:
+        print("no reverse")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # so address can be reused after termination of program
+    s.bind(('', port))
+    s.listen(1) # listening socket s
+    while True:
+        print("Waiting for new connection...")
+        (clientsocket, address) = s.accept() # clientsocket, another socket structure
+        while True:
+            try:
+                z = clientsocket.recv(1024)
+                if len(z) == 0:
+                    break
+                if(reverse):
+                    print(z[::-1])
+                    clientsocket.send(z[::-1])
+                else:
+                    print(z)
+                    clientsocket.send(z)
+            except ConnectionResetError:
+                print("Connection reset by peer.")
+                break
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', dest='port', help='port on which service will listen on', required=True, type=int)
+    parser.add_argument('-r', '--reverse', dest='reverse', help='reverse the message sent through server', action='store_true')
+    args = parser.parse_args()
+
+    echo_service(args.port, args.reverse)
---
+++ mp3/basicIMclient.py
@@ -0,0 +1,52 @@
+import socket
+import sys
+import argparse
+import select
+import broadcastMsg_pb2
+
+
+def basicIMclient(servername, nickname):
+    #print("servername: %s | nickname: %s" % (servername, nickname))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((servername, 9999))
+
+    # here are the things we want to read from
+    read_handles = [ sys.stdin, sock ]
+
+    send_message = broadcastMsg_pb2.Message()
+    send_message.nickname = nickname
+    rcv_message = broadcastMsg_pb2.Message()
+
+    while True:
+        ready_to_read_list, _, _ = select.select(read_handles, [], [])
+        
+        if sys.stdin in ready_to_read_list:
+            # new data from STDIN
+            user_input = input()
+            if user_input.lower().strip() == "exit":
+                exit(0)
+            elif len(user_input) != 0:
+                # encode input along with nickname using proto buffer
+                send_message.message = user_input
+                sock.send(send_message.SerializeToString())
+
+        if sock in ready_to_read_list:
+            # we have new data from the network!
+            data = sock.recv(1280)
+            rcv_message = rcv_message.FromString(data)
+            if len(data) == 0:
+                exit(0)
+            print("%s: %s" % (rcv_message.nickname, rcv_message.message))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--servername', dest='servername', help='IP or hostname of the machine that is already running the BasicIM server', required=True)
+    parser.add_argument('-n', '--nickname', dest='nickname', help='nickname or alias chosen by the user', required=True)
+    args = parser.parse_args()
+
+    try:
+        basicIMclient(args.servername, args.nickname)
+    except KeyboardInterrupt:
+        exit(0)
